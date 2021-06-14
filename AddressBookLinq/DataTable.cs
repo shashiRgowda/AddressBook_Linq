95104743102b66e8edd908f10eafadb11af8d35d
shashiRgowda <shashikumartr64@gmail.com>
2021-06-14 21:08:38 +0530

    Added:UC1_CreateDataTable

---
+++ AddressBookLinq/DataTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookService_Linq
+{
+    class DataTable
+    {
+        public void AddToDataTable()
+        {
+            DataTable table = new DataTable();
+        }
+    }
+}
---
+++ AddressBookLinq/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBookService_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address book linq problem!");
+            AddressBook Service = new AddressBook();
+        }
+    }
+}
