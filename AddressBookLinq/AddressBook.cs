0e8b697b404b3e000fe8309e1985ed85c3ce19bf
shashiRgowda <shashikumartr64@gmail.com>
2021-06-14 21:17:09 +0530

    Added:UC3_InsertConatctDetails

--- AddressBookLinq/AddressBook.cs
+++ AddressBookLinq/AddressBook.cs
@@ -1,16 +1,15 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Text;
 
-namespace AddressBookLinq
+namespace AddressBookService_Linq
 {
     class AddressBook
     {
-        DataTable table = new DataTable("AddressBook");
-        public AddressBook()
+        public DataTable AddressBookDataTable()
         {
+            DataTable table = new DataTable();
+
             table.Columns.Add("FirstName", typeof(string));
             table.Columns.Add("LastName", typeof(string));
             table.Columns.Add("Address", typeof(string));
@@ -19,6 +18,12 @@ namespace AddressBookLinq
             table.Columns.Add("Zip", typeof(string));
             table.Columns.Add("PhoneNumber", typeof(string));
             table.Columns.Add("Email", typeof(string));
+
+            table.Rows.Add("Shashi", "kumar", "shanthinagar", "Kolar", "Karnataka", "563101", "8884540867", "shashi@gmail.com");
+            table.Rows.Add("Sharath", "Gowdru", "RT nagar", "Hasan", "Karnatka", "563102", "8660907461", "sharath@gmail.com");
+            table.Rows.Add("Nandan", "Kumar", "HSR layout", "chennai", "AndraPradesh", "563103", "8197505915", "nandan@gmail.com");
+            table.Rows.Add("Shalini", "Reddy", "Indhirangar", "Banglore", "Karnataka", "560057", "8710897106", "shalini@gmail.com");
+            return table;
         }
     }
 }
